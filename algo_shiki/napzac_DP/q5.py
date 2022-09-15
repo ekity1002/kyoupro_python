eeca2746479c308a10273377482d893cc08a88bf
ekity1002 <dreamguidance1@gmail.com>
2022-09-16 07:17:48 +0900

    update DP

--- algo_shiki/napzac_DP/q5.py
+++ algo_shiki/napzac_DP/q5.py
@@ -1,17 +1,22 @@
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-dp = [[0]* (M+1) for _ in range(N+1)]
-dp[0][0] = 1
+dp = [[-1]* (M+1) for _ in range(N+1)]
+dp[0][0] = 0 
 
 for i in range(N):
     for j in range(M+1):
+        if dp[i][j] < 0: continue
+
         # A[i] 選ばない場合
-        dp[i+1][j] += dp[i][j]
+        if dp[i+1][j] >= 0:
+            dp[i+1][j] = min(dp[i][j], dp[i+1][j])
+        else:
+            dp[i+1][j] = dp[i][j]
 
         # 選ぶ場合
         if j + A[i] <= M:
-            dp[i+1][j+A[i]] += dp[i][j]
+            dp[i+1][j+A[i]] = dp[i][j] + 1
 # from pprint import pprint
 # pprint(dp)
 # ans = 0
@@ -19,4 +24,4 @@ for i in range(N):
 #     if dp[i][-1] >= 0:
 #         ans += dp[i][-1]
 # print(ans)
-print(dp[-1][-1] % 1000)
+print(dp[-1][-1])
