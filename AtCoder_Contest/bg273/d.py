3f5862eaa7ac39637cb7cfb7a056f74f897cd92b
ekity1002 <dreamguidance1@gmail.com>
2022-11-05 14:24:53 +0900

    add

--- AtCoder_Contest/bg273/d.py
+++ AtCoder_Contest/bg273/d.py
@@ -1,13 +1,15 @@
 # D - LRUD Instructions
 from collections import defaultdict
 
+from bisect import bisect_left
+
 H, W, rs, cs = map(int, input().split())
 N = int(input())
 
 wall = defaultdict(int)  # 壁情報
 
-w_list = [[0] for _ in range(N)]  # 横壁 index list
-h_list = [[0] for _ in range(N)]  # 立壁 index list
+w_list = defaultdict(lambda: [0])  # 横壁 index list
+h_list = defaultdict(lambda: [0])  # 横壁 index list
 
 for i in range(N):
     r, c = map(int, input().split())
@@ -15,33 +17,92 @@ for i in range(N):
     w_list[r].append(c)
     h_list[c].append(r)
 
-w_list.append(W + 1)
-h_list.append(H + 1)
+for k, v in w_list.items():
+    w_list[k].append(W + 1)
+    w_list[k].sort()
+for k, v in h_list.items():
+    h_list[k].append(H + 1)
+    h_list[k].sort()
 
+print(w_list)
 Q = int(input())
 current = (rs, cs)
 
+from pdb import set_trace as st
 
-def move(current, d):
+
+def move(current, d, l):
+    # 動かす方向を見る
+    # その方向の壁の indexを取得
     r, c = current
+    st()
     if d == "L":
-        new_current = (r, c - 1)
+        idx = bisect_left(
+            w_list[r],
+            c,
+        )
+        # 壁の座標
+        if idx == 0:
+            wall_c = 0
+        else:
+            wall_c = w_list[r][idx]
+
+        if c - l <= wall_c:
+            # 途中で壁に当たる場合、壁の手前の座標
+            new_current = (r, wall_c + 1)
+        else:
+            new_current = (r, c - l)
     elif d == "R":
-        new_current = (r, c + 1)
+        idx = bisect_left(
+            w_list[r],
+            c,
+        )
+        # 壁の座標
+        if idx == 0:
+            wall_c = W + 1
+        else:
+            wall_c = w_list[r][idx]
+        if c + l >= wall_c:
+            # 途中で壁に当たる場合、壁の手前の座標
+            new_current = (r, wall_c - 1)
+        else:
+            new_current = (r, c + l)
     elif d == "U":
-        new_current = (r - 1, c)
+        idx = bisect_left(
+            h_list[c],
+            r,
+        )
+        # 壁の座標
+        if idx == 0:
+            wall_r = 0
+        else:
+            wall_r = h_list[c][idx]
+
+        if r - l <= wall_r:
+            # 途中で壁に当たる場合、壁の手前の座標
+            new_current = (wall_r + 1, c)
+        else:
+            new_current = (r - l, c)
     elif d == "D":
-        new_current = (r + 1, c)
+        idx = bisect_left(
+            h_list[c],
+            r,
+        )
+        # 壁の座標
+        if idx == 0:
+            wall_r = H + 1
+        else:
+            wall_r = h_list[c][idx]
 
-    is_wall = wall[new_current]
-    if is_wall:
-        return current
+        # 壁の座標
+        wall_r, wall_c = h_list[c][idx], w_list[r][c]
+        if r + l >= wall_r:
+            # 途中で壁に当たる場合、壁の手前の座標
+            new_current = (wall_r - 1, c)
+        else:
+            new_current = (r + l, c)
 
-    # はみ出していないか
-    if 1 <= new_current[0] <= H and 1 <= new_current[1] <= W:
-        return new_current
-
-    return current
+    return new_current
 
 
 ans = []
@@ -49,20 +110,9 @@ for _ in range(Q):
     d, l = input().split()
     l = int(l)
 
-    # 動かす方向を見る
-    # その方向の壁の indexを取得
-    # L or R: H[] から bisect_left でcurrent の c がはいる index
-    # U or D W[] から bisect_left でcurrent の r がはいる index
-    # index 1以上 H以下 壁の一つ手前の座標に更新
-    # 最も近い壁 の 座標取得
-    # もっとも 近い壁の手前に current を更新
-    for i in range(l):
-        new_current = move(current, d)
-        if current == new_current:
-            break
-        current = new_current
+    new_current = move(current, d, l)
     # マスを出力
-    print(*current)
-    # ans.append(current)
+    # print(*current)
+    ans.append(new_current)
 
-# [print(*a) for a in ans]
+[print(*a) for a in ans]
